a93ab95b7fb1add4d6f2b9aa9a999d9aa2c8fbc6
David Hau <davidphiliphk@gmail.com>
2017-09-07 00:10:04 +0800

    Handle View and Editable View table

--- themes/impact_theme/templates/jirehhk/views-view-table--one-time-serving-assignment--page.tpl.php
+++ themes/impact_theme/templates/jirehhk/views-view-table--one-time-serving-assignment--page.tpl.php
@@ -18,6 +18,66 @@
  *   field id, then row number. This matches the index in $rows.
  * @ingroup views_templates
  */
+
+$color_warning = "LemonChiffon";
+$color_error = "LightPink";
+
+// Used for calculating conflicted post
+$nids = get_nid_array_by_rows($rows);
+$assignNodes = array();
+foreach ($nids as $nid) {
+    $assignNodes[$nid] = (array)node_load($nid);
+}
+$lang = end($assignNodes)['language'];
+
+$termsCache = cache_get('one_time_serving_post_term_cache', 'cache');
+if ($termsCache) 
+{
+    $terms = $termsCache->data;
+} else {
+    cache_set('one_time_serving_post_term_cache', jireh_serving_assignment_get_term_entity_array('one_time_serving_post'), 'cache', time() + 60);
+    $terms = cache_get('one_time_serving_post_term_cache', 'cache')->data;
+}
+
+
+$postTidToNameArr = array();
+foreach ($terms as $term) {
+    $postTidToNameArr[$term->tid] = $term->name;
+}
+
+$conflictedPostArrays = array();
+// for each assignment
+foreach ($assignNodes as $nid => $assignNode) {
+    $servantToPostArray = array();
+    // for each post
+    foreach ($assignNode as $field => $value) {
+        if (preg_match ('/^field_/', $field) && isset($value[$lang][0]['tid']) && in_array($header[$field], $postTidToNameArr)) {
+            $servingPostName = $header[$field];
+            $servant_tid = $value[$lang][0]['tid'];
+            if (!isset($servantToPostArray[$servant_tid]))
+                $servantToPostArray[$servant_tid] = array();
+            $servantToPostArray[$servant_tid][] = $servingPostName;
+        }
+    }
+    
+    // Find all corresponding conflicted posts for each assigned post
+    $conflictedPostArrays[$nid] = jireh_serving_assignment_get_conflicted_post_arr($servantToPostArray, $lang, $terms);
+}
+
+function get_nid_array_by_rows(&$rows) {
+    $nids = array();
+    foreach ($rows as $row_count => $row) {
+        $p = xml_parser_create();
+        xml_parse_into_struct($p, $row['title'], $vals, $index);
+        $href = $vals[0]['attributes']['HREF'];
+        $href_element = preg_split('/\//', $href);
+        $nids[$row_count] = end($href_element);
+        xml_parser_free($p);
+    }
+    
+    return $nids;
+}
+
 ?>
 <table <?php if ($classes) { print 'class="'. $classes . '" '; } ?><?php print $attributes; ?>>
    <?php if (!empty($title) || !empty($caption)) : ?>
@@ -36,12 +96,22 @@
   <?php endif; ?>
   <tbody>
     <?php foreach ($rows as $row_count => $row): ?>
-      <tr <?php if ($row_classes[$row_count]) { print 'class="' . implode(' ', $row_classes[$row_count]) .'"';  } ?>>
-        <?php foreach ($row as $field => $content): ?>
-          <td <?php if ($field_classes[$field][$row_count]) { print 'class="'. $field_classes[$field][$row_count] . '" '; } ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
+      <tr <?php if ($row_classes[$row_count]) { print 'class="' . implode(' ', $row_classes[$row_count]) .'"'; } ?>>
+        <?php 
+        $nid = $nids[$row_count];
+        foreach ($row as $field => $content) {?>
+          <td <?php if ($field_classes[$field][$row_count]) {
+              print 'class="'. $field_classes[$field][$row_count] . '" ';
+              if (isset($conflictedPostArrays[$nid][$header[$field]]) && !empty($conflictedPostArrays[$nid][$header[$field]])) {
+                  print ' bgcolor="' . $color_warning . '" ';
+                  $correspondingConflictedPosts = $conflictedPostArrays[$nid][$header[$field]];
+                  $conflictMsg = 'Conflicted with: ' . implode(", ", $correspondingConflictedPosts);
+                  print ' title="' . $conflictMsg . '" ';
+              }
+          } ?><?php print drupal_attributes($field_attributes[$field][$row_count]); ?>>
             <?php print $content; ?>
           </td>
-        <?php endforeach; ?>
+        <?php } ?>
       </tr>
     <?php endforeach; ?>
   </tbody>
